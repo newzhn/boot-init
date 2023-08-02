6cd5ccc92b4376720fd8a5bd205114e1c3a3f0dc
zhn <zhanhaonan1@163.com>
2023-08-02 21:00:05 +0800

    first commit

---
+++ src/test/java/com/zhn/bootinit/BootInitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhn.bootinit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootInitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
